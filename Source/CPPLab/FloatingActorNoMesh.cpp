11dc8c26bdb0235eaaf7bca059c71a4ab6e2d522
Mcflaber <danielaramirez4949@gmail.com>
2026-01-23 17:01:48 -0600

    lab02 Complete

--- Source/CPPLab/FloatingActorNoMesh.cpp
+++ Source/CPPLab/FloatingActorNoMesh.cpp
@@ -9,7 +9,8 @@ AFloatingActorNoMesh::AFloatingActorNoMesh()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
-
+	DefaultSceneRoot = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
+	DefaultSceneRoot->SetupAttachment(RootComponent);
 
 }
 
--- Source/CPPLab/FloatingActorNoMesh.h
+++ Source/CPPLab/FloatingActorNoMesh.h
@@ -23,4 +23,6 @@ public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category = "Default")
+	USceneComponent* DefaultSceneRoot;
 };
