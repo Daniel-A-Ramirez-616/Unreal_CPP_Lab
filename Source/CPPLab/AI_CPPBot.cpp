e8ad99b1d83f31afbc823d406f53f88188a71148
Daniel <danielaramirez4949@gmail.com>
2026-03-17 13:48:10 -0500

    lab08 started

---
+++ Source/CPPLab/AI_CPPBot.cpp
@@ -0,0 +1,48 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AI_CPPBot.h"
+
+// Sets default values
+AAI_CPPBot::AAI_CPPBot()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AAI_CPPBot::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AAI_CPPBot::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AAI_CPPBot::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
+void MoveToPlayer()
+{
+
+}
+
+void MoveToPlayerAgain() 
+{
+
+}
+
+void WaitToMoveAgain()
+{
+
+}
